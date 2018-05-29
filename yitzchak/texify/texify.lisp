e9fa3d2d37e54f720e44923d86247b30ef0679b7
Tarn Burton <twburton@gmail.com>
2018-05-29 16:44:59 -0400

    Add license

--- yitzchak/texify/texify.lisp
+++ yitzchak/texify/texify.lisp
@@ -1,3 +1,19 @@
+;; texify.lisp -- Cascading style TeX/LaTeX code of Maxima expressions.
+;; Copyright (C) 2018 by Tarn W. Burton
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (in-package :maxima)
 
 (defvar $texify_styles '((mlist simp) $latex $tex_prefix_functions $tex))
