7e11b87c5ed911a0246de159c778ab5c336d2cdc
Robert Dodier <robert_dodier@users.sourceforge.net>
2019-01-13 16:42:05 -0800

    Rename low_discrepancy_foo to cacm647_foo throughout.

--- robert-dodier/low_discrepancy/low_discrepancy-index.lisp
+++ robert-dodier/low_discrepancy/low_discrepancy-index.lisp
@@ -2,12 +2,12 @@
 (let (
 (deffn-defvr-pairs '(
 ; CONTENT: (<INDEX TOPIC> . (<FILENAME> <BYTE OFFSET> <LENGTH IN CHARACTERS> <NODE NAME>))
-("low_discrepancy_makelist_faure" . ("low_discrepancy.info" 1749 174 "Definitions for package low_discrepancy"))
-("low_discrepancy_makelist_halton" . ("low_discrepancy.info" 1924 178 "Definitions for package low_discrepancy"))
+("cacm647_makelist_faure" . ("low_discrepancy.info" 1749 166 "Definitions for package low_discrepancy"))
+("cacm647_makelist_halton" . ("low_discrepancy.info" 1916 170 "Definitions for package low_discrepancy"))
 ))
 (section-pairs '(
 ; CONTENT: (<NODE NAME> . (<FILENAME> <BYTE OFFSET> <LENGTH IN CHARACTERS>))
-("Definitions for package low_discrepancy" . ("low_discrepancy.info" 1661 441))
+("Definitions for package low_discrepancy" . ("low_discrepancy.info" 1661 425))
 ("Introduction to package low_discrepancy" . ("low_discrepancy.info" 781 708))
 )))
 (load-info-hashtables (maxima::maxima-load-pathname-directory) deffn-defvr-pairs section-pairs))
