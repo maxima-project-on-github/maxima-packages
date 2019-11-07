1d84d3c64f75ef5124d5ac694fa85d157e835cb2
Robert Dodier <robert_dodier@users.sourceforge.net>
2019-11-06 21:42:51 -0800

    In package lexical_symbols, redefine function definition operator ":=" so that gensyms are substituted for function arguments.

--- robert-dodier/lexical_scope/blex.lisp
+++ robert-dodier/lexical_scope/blex.lisp
@@ -1,5 +1,5 @@
 ;; blex.lisp -- lexical block for Maxima
-;; copyright 2012 by Robert Dodier
+;; copyright 2012, 2019 by Robert Dodier
 ;; I release this work under terms of the GNU GPL
 ;;
 ;; examples:
@@ -25,3 +25,20 @@
     (gensym-mprog `((mprog) ((mlist) ,@ (append vars-only-gensyms gensym-inits)) ,@ (cdr gensym-mprogn))))
    (meval gensym-mprog)))
 
+;; Redefine function definition operator ":=" so that gensyms are substituted for function arguments.
+;; This is a simple-minded experiment to investigate lexical scope for function arguments.
+
+(let ((default-mdefine-mfexpr* (get 'mdefine 'mfexpr*)))
+  (setf (get 'mdefine 'mfexpr*)
+        #'(lambda (e)
+            ;; (format t "HEY E = ~S~%" e)
+            (let*
+              ((args (rest ($listofvars (second e))))
+               (args-gensyms (mapcar
+                               #'(lambda (s)
+                                   (let ((s1 (gensym)))
+                                     (setf (get s1 'reversealias) (or (get s 'reversealias) s)) s1)) args))
+               (subst-eqns (mapcar #'(lambda (x y) `((mequal) ,x ,y)) args args-gensyms))
+               (gensym-expr ($psubstitute `((mlist) ,@ subst-eqns) e)))
+              ;; (format t "HEY NOW CALL DEFAULT MDEFINE MFEXPR* ON ~S~%" gensym-expr)
+              (funcall default-mdefine-mfexpr* gensym-expr)))))
