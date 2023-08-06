e6abc4b48529233d89940855de9bdd650b3fde9a
Robert Dodier <robert.dodier@gmail.com>
2023-08-05 21:20:25 -0700

    (1) In expressions_from_xml, be more careful about empty elements. (2) New function extract_ops which finds the subexpressions which have a given operator.

--- robert-dodier/from_wxmx/expressions_from_xml.lisp
+++ robert-dodier/from_wxmx/expressions_from_xml.lisp
@@ -4,6 +4,12 @@
 
 (ql:quickload :plump)
 
+;; about elements vs nodes, see for example:
+;; https://stackoverflow.com/questions/132564/whats-the-difference-between-an-element-and-a-node-in-xml
+;;
+;; apparently element is the only kind of node which can contain child nodes and attributes.
+;; probably need to think more carefully about elements, nodes, children, etc.
+
 (defmfun $expressions_from_xml (x)
   (cond
     ((plump:textual-node-p x)
@@ -17,9 +23,14 @@
         (children-exprs (mapcar (symbol-function '$expressions_from_xml) children-list)))
        (if children-exprs
          `((mqapply) ((,op) ,@attribute-values) ,@children-exprs)
-         `((mqapply) ((,op) ,@attribute-values) ,(funcall (symbol-function '$expressions_from_xml) (plump:first-child x))))))
+         (if (plump:first-child x)
+           (let ((first-child-expr (funcall (symbol-function '$expressions_from_xml) (plump:first-child x))))
+             `((mqapply) ((,op) ,@attribute-values) ,first-child-expr))
+           `((mqapply) ((,op) ,@attribute-values))))))
+     (x
+       (merror "expressions_from_xml: nonnull argument ~A is not a text or nesting node in an XML parse tree." x))
      (t
-       (merror "expressions_from_xml: argument is not a text or nesting node in an XML parse tree."))))
+       (merror "expressions_from_xml: null argument; how did that happen?"))))
 
 (defmfun $parse_xml (f)
   (plump:first-child
