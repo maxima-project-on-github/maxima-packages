f472547a173398c20a8bafaf12b16797803e3f5c
Robert Dodier <robert.dodier@gmail.com>
2025-01-19 21:47:56 -0800

    distribute_over_tranches: write results from child processes back to parent, parent prints results.

--- robert-dodier/distribute_over_tranches/distribute_over_tranches.lisp
+++ robert-dodier/distribute_over_tranches/distribute_over_tranches.lisp
@@ -14,18 +14,43 @@
   (let*
     ((ii (seq-1-length m))
      (ii-tranches (divide-list-into-tranches ii n))
-     child-pids)
+     child-pids
+     fds-for-parent-to-read)
 
     (dotimes (i n)
-      (let ((indices-to-process (nth i ii-tranches)))
+      (let
+        ((indices-to-process (nth i ii-tranches))
+         (fd-pair (multiple-value-list (sb-posix:pipe))))
+        (format t "for tranche ~d, fd pair = ~a~%" i fd-pair)
         (let ((child-pid (sb-posix:fork)))
           (if (eql child-pid 0)
             (progn
-              (mapcar (lambda (j) (meval (maxima-substitute j expr-loop-var expr))) indices-to-process)
+              (sb-posix:close (first fd-pair))
+              (let*
+                ((f (lambda (j) (meval (maxima-substitute j expr-loop-var expr))))
+                 (g (lambda (j) (mfuncall '$string (funcall f j))))
+                 (g-f-j (mapcar g indices-to-process)))
+                (mapcar (lambda (s) (sb-posix:write (second fd-pair) (sb-sys:vector-sap s) (* 4 (length s)))) g-f-j))
+              (sb-posix:close (second fd-pair))
               (sb-posix:exit 0))
             (progn
               (format t "process ~d given indices [~{~a~^, ~}] to process~%" child-pid indices-to-process)
-              (push child-pid child-pids))))))
+              (push child-pid child-pids)
+              (sb-posix:close (second fd-pair))
+              (format t "save fd ~d to read output of process ~d~%" (first fd-pair) child-pid)
+              (push (first fd-pair) fds-for-parent-to-read))))))
+
+    (setq fds-for-parent-to-read (reverse fds-for-parent-to-read))
+
+    (dotimes (i n)
+      (format t "read from fd ~d for process ~d~%" (nth i fds-for-parent-to-read) (nth i child-pids))
+      (let*
+        ((buf (make-array 16384 :element-type 'standard-char))
+         (n-bytes (sb-posix:read (nth i fds-for-parent-to-read) (sb-sys:vector-sap buf) 16384)))
+        (loop while (> n-bytes 0) do
+              (format t "read ~d bytes from process ~d: ~a~%" n-bytes (nth i child-pids) buf)
+              (setq n-bytes (sb-posix:read (nth i fds-for-parent-to-read) (sb-sys:vector-sap buf) 16384))))
+      (sb-posix:close (nth i fds-for-parent-to-read)))
 
     (dotimes (i n)
       (sb-posix:waitpid (nth i child-pids) 0))))
